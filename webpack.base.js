1bc83f9e0db026f0a3759fba4e4ca649532169ad
Hendrik Araujo <jhendrik@gmail.com>
2018-06-18 01:46:12 -0300

    Inclusão de alias no webpack

--- webpack.base.js
+++ webpack.base.js
@@ -38,6 +38,11 @@ module.exports = {
   },
 
   resolve: {
-    extensions: ['.js', '.jsx']
+    extensions: ['.js', '.jsx'],
+    alias: {
+      src: path.join(__dirname, 'src'),
+      components: path.join(__dirname, 'src', 'components'),
+      sass: path.join(__dirname, 'src', 'sass')
+    }
   }
 }
