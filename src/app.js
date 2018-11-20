75af18bbafdb4eab07dafe695cd638b1620403b1
Hendrik Araujo <jhendrik@gmail.com>
2018-11-19 22:46:35 -0200

    Ajust react-hot-loader

--- src/app.js
+++ src/app.js
@@ -1,20 +1,15 @@
 'use strict'
 
 import React from 'react'
-import { hot } from 'react-hot-loader'
 
 import 'sass/app.scss'
 import reactLogo from 'images/react-logo.svg'
 
-class App extends React.Component {
-  render () {
-    return (
-      <div className='container'>
-        <img src={reactLogo} style={{ width: '40px', height: '40px' }} />
-        My App
-      </div>
-    )
-  }
-}
+const App = () => (
+  <div className='container'>
+    <img src={reactLogo} style={{ width: '40px', height: '40px' }} />
+    My App
+  </div>
+)
 
-export default hot(module)(App)
+export default App
--- src/index.js
+++ src/index.js
@@ -1,7 +1,22 @@
 'use strict'
 
 import React from 'react'
-import { render } from 'react-dom'
+import ReactDOM from 'react-dom'
+import { AppContainer } from 'react-hot-loader'
+
 import App from './app'
 
-render(<App />, document.querySelector('[data-js="app"]'))
+const render = Component => {
+  ReactDOM.render(
+    <AppContainer>
+      <Component />
+    </AppContainer>,
+    document.querySelector('[data-js="app"]')
+  )
+}
+
+render(App)
+
+if (module.hot) {
+  module.hot.accept('./app', () => { render(App) })
+}
